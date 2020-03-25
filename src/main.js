86e04e1daefc41e8aa7ab189dfbcd47c61dd8275
Aaron Collegeman <aaron@c21redwood.com>
2020-03-25 00:07:04 -0400

    init

--- src/main.js
+++ src/main.js
@@ -1,10 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
 
+
+// import openemoji from './assets/openmoji.json'
+// import dice from './assets/dice.json'
+
+const reqSvgs = require.context('./assets/openmoji-svg-color', true, /\.svg$/)
+const svgs = reqSvgs.keys().reduce((images, path) => {
+  images[path] = reqSvgs(path)
+  return images
+}, {})
+
+console.log(svgs)
+
 Vue.config.productionTip = false
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 
 new Vue({
   router,
---
+++ vue.config.js
@@ -0,0 +1,17 @@
+const vueConfig = {
+  publicPath: ''
+}
+
+if (process.env.NODE_ENV === 'production') {
+  vueConfig.chainWebpack = (config) => {
+    config.plugin('html').init((Plugin, args) => {
+      const newArgs = {
+        ...args[0],
+      }
+      newArgs.minify.removeAttributeQuotes = false
+      return new Plugin(newArgs)
+    });
+  }
+}
+
+module.exports = vueConfig
